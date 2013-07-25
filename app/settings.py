2fd8ea46fc75ae90495440873278ff563dcff067
Stephen Young <stephen@tryllo.com>
2013-07-25 12:36:35 -0400

    adding requirements.txt file

--- app/settings.py
+++ app/settings.py
@@ -60,15 +60,15 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/motsditsquebec.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/Users/stephen/devel/contracts/motsditsquebec/motsdits/assets',
+    '/Users/stephen/devel/contracts/motsditsquebec/motsdits/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
