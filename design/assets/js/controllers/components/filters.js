e31cb31df18f16771af34c0ed20f98a6dc1a9075
Stephen Young <me@hownowstephen.com>
2013-12-05 15:57:31 -0500

    first bit of logic for subfilters

--- design/assets/js/controllers/components/filters.js
+++ design/assets/js/controllers/components/filters.js
@@ -6,7 +6,11 @@ angular.module('MotsDitsQuebec').controller('FilterCtrl', function($rootScope, $
 
   // Set the default filter
   $scope.active_category = {"id": 0, "name": "Toutes Categories"};
+  $scope.active_subfilters = {};
+
+  $scope.available_subfilters = {};
   $scope.categories = [$scope.active_category];
+  $scope.hide_menus = false;
 
   // Set up a subfilter
 
@@ -16,24 +20,57 @@ angular.module('MotsDitsQuebec').controller('FilterCtrl', function($rootScope, $
   });
 
   /**
+   * Hides menus after changing category
+   */
+  $scope.hideMenus = function(){
+    $scope.hide_menus = true;
+    setTimeout(function(){
+      $scope.$apply(function() {
+        $scope.hide_menus = false;
+      });
+    }, 500);
+  };
+
+  /**
    * Choose a category to show
    */
   $scope.showCategory = function(category){
+
+    $scope.hideMenus();
+
     // Emit the event
     $rootScope.$broadcast("categoryFilterEvent", category);
 
-    for(var i=0; i < $scope.categories.length; i ++){
-      if($scope.categories[i].id == category.id){
-        $scope.active_category = $scope.categories[i];
-      }
-    }
+    // Set the active category
+
+    // Set up the new filter
+    $scope.active_category = category;
+    $scope.active_subfilters = {};
+
+    // Set up subfilters
+    $scope.available_subfilters = {};
+
+    // Populate the available subfilters
+    angular.forEach($scope.active_category.subfilters, function(subfilter){
+      if(!$scope.available_subfilters[subfilter.subfilter_type])
+        $scope.available_subfilters[subfilter.subfilter_type] = {name: subfilter.subfilter_type, subfilters: []};
+      $scope.available_subfilters[subfilter.subfilter_type].subfilters.push(subfilter);
+    });
+
   };
+
 
   /**
    * Choose a subfilter to show
    * @TODO implement
    */
-  $scope.setSubfilter = function(subfilter){
+  $scope.setSubfilter = function(type, subfilter){
+
+    $scope.active_subfilters[type] = subfilter;
+
+    // Dispatch a subfilter event
+    $rootScope.$broadcast("categorySubFilterEvent", {category: $scope.active_category, subfilters: $scope.active_subfilters});
+
   };
 
   /** 
