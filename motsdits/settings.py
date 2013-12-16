c969cdf5a3f20c20c750d5821883d8a26d5ee8c3
Stephen Young <me@hownowstephen.com>
2013-12-16 01:41:29 -0500

    no need for GEOS

--- motsdits/settings.py
+++ motsdits/settings.py
@@ -178,7 +178,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
 
     # Administration
     'django.contrib.admin',
